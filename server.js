ab8f765cf8fc800b8f8c21c390dfcf1455a06f16
Maxwell Krause <maxwell.l.krause@gmail.com>
2016-02-26 14:22:32 -0500

    trying to fix responsiveness on countdown class

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var port = process.env.PORT || 3001;
+var app = express();
+var http = require('http').Server(app);
+
+
+
+app.use(express.static(__dirname + "/public"));
+app.use('/bootstrap', express.static(__dirname + "/node_modules/bootstrap/dist/css/"));
+app.use('/jquery', express.static(__dirname + "/node_modules/jquery/dist/"));
+app.use('/scrollMagic', express.static(__dirname + "/node_modules/scrollmagic/scrollmagic/"));
+
+
+app.get('/', function(request, response, next){
+  response.sendFile('app');
+});
+
+
+http.listen(port);
